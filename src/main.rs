595d2e18b12999cb5c7d1a7d75a7b7a3778675b1
GlitchBanger <ayushsubramanyamnayam@gmail.com>
2023-02-15 23:40:21 +0530

    message

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use bevy::prelude::*;
+use bevy::window::WindowMode;
+
+fn main() {
+    App::new()
+    .insert_resource(WindowDescriptor {
+        mode: WindowMode::BorderlessFullscreen,
+        ..default()
+    })
+    .add_plugins(DefaultPlugins)
+    .add_startup_system(setup)
+    .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.3, 0.7, 0.3),
+                ..default()
+            },
+            transform: Transform {
+                scale: Vec3::new(96.0, 96.0, 10.0),
+                ..default()
+            },
+            ..default()
+        }
+    );
+}
