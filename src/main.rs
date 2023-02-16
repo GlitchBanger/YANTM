80dbf3002dfb59f27a8d27fd1127131a500d5551
GlitchBanger <ayushsubramanyamnayam@gmail.com>
2023-02-16 22:07:22 +0530

    Added road and house

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+use bevy::core::FixedTimestep;
 use bevy::prelude::*;
 use bevy::window::WindowMode;
 
 #[derive(Component)]
 struct Protagonist;
+
+#[derive(Component)]
+struct Road;
+
+#[derive(Component)]
+struct House;
 
 fn main() {
     App::new()
@@ -12,12 +19,19 @@ fn main() {
     })
     .add_plugins(DefaultPlugins)
     .add_startup_system(setup)
-    .add_system(transforming)
+    .add_startup_system(add_road)
+    .add_startup_system(add_houses)
+    .add_system_set(
+        SystemSet::new()
+            .with_run_criteria(FixedTimestep::step(0.04))
+            .with_system(transforming)
+    )
     .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, windows: Res<Windows>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    let window = windows.get_primary().unwrap();
     commands.spawn_bundle(
         SpriteBundle {
             sprite: Sprite {
@@ -26,6 +40,7 @@ fn setup(mut commands: Commands) {
             },
             transform: Transform {
                 scale: Vec3::new(96.0, 96.0, 10.0),
+                translation: Vec3::new(-700.0, 100.0 - window.width()/4.0, 10.0),
                 ..default()
             },
             ..default()
@@ -36,6 +51,50 @@ fn setup(mut commands: Commands) {
 
 fn transforming(mut query: Query<&mut Transform, With<Protagonist>>) {
     for mut transform in query.iter_mut() {
-        transform.translation.x += 10.0;
+        transform.translation.x += 1.5;
     }
+}
+
+// fn resize_notificator(resize_event: Res<Events<WindowResized>>) {
+//     let mut reader = resize_event.get_reader();
+//     for e in reader.iter(&resize_event) {
+//         println!("width = {} height = {}", e.width, e.height);
+//     }
+// }
+fn add_road(windows: Res<Windows>, mut commands: Commands) {
+    let window = windows.get_primary().unwrap();
+    println!("Window size was: {},{}", window.width(), window.height());
+    commands.spawn_bundle(
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.0, 0.0, 0.0),
+                ..default()
+            },
+            transform: Transform { 
+                scale: Vec3::new(window.width() * 2.0, 100.0, 10.0),
+                translation: Vec3::new(0.0, -window.height()/4.0, 0.0),
+                ..default()
+            },
+            ..default()
+        }
+    )
+    .insert(Road);
+}
+
+fn add_houses(mut commands: Commands) {
+    commands.spawn_bundle(
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(1.0, 0.0, 0.0),
+                ..default()
+            },
+            transform: Transform {
+                scale: Vec3::new(200.0, 200.0, 1.0),
+                translation: Vec3::new(-450.0, 0.0, 0.0),
+                ..default()
+            },
+            ..default()
+        }
+    )
+    .insert(House);
 }
