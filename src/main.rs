a379c8b3f44b52fd21af63f820b9da31ba86ed97
GlitchBanger <ayushsubramanyamnayam@gmail.com>
2023-02-20 19:07:57 +0530

    Added code to get window aspect

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
-use bevy::core::FixedTimestep;
+use bevy::core::{FixedTimestep};
 use bevy::prelude::*;
-use bevy::window::WindowMode;
+use bevy::window::{WindowMode};
+use display_info::DisplayInfo;
 
+struct Window {
+    width: f32,
+    height: f32
+}
+
+trait States {}
+
+struct MainScreen;
+struct Scene1;
+
+impl States for MainScreen {}
+
+impl States for Scene1 {}
+
+struct GameState<T: States> {
+    current: T,
+    previous: T
+}
+
+//scene 1 spritelist
 #[derive(Component)]
 struct Protagonist;
 
@@ -17,13 +38,23 @@ struct Dustbin;
 #[derive(Component)]
 struct Beggar;
 
+#[derive(Component)]
+struct SceneText;
+
 fn main() {
+
+    let display_info = DisplayInfo::all().unwrap()[0];
+
     App::new()
     .insert_resource(WindowDescriptor {
         mode: WindowMode::BorderlessFullscreen,
         ..default()
     })
     .add_plugins(DefaultPlugins)
+    .insert_resource(Window {
+        width: display_info.width as f32, 
+        height: display_info.height as f32
+    })
     .add_startup_system(setup)
     .add_startup_system(add_road)
     .add_startup_system(add_houses)
@@ -34,12 +65,41 @@ fn main() {
             .with_run_criteria(FixedTimestep::step(0.04))
             .with_system(transforming)
     )
+    .add_system_set(
+        SystemSet::new()
+            .with_run_criteria(FixedTimestep::step(0.04))
+            .with_system(beggar_movements)
+    )
     .run();
 }
 
-fn setup(mut commands: Commands, windows: Res<Windows>) {
+//sets up only the protagonists and the camera maybe even the ui bundle
+fn setup(mut commands: Commands, asset: Res<AssetServer>, window: Res<Window>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    let window = windows.get_primary().unwrap();
+    commands.spawn_bundle(UiCameraBundle::default());
+    commands.spawn_bundle(Text2dBundle {
+        text: Text::with_section(
+            "Good day for meals", 
+            TextStyle { 
+                font_size: 30.0, 
+                color: Color::rgb(1.0, 1.0, 1.0), 
+                font: asset.load("Pixeled.ttf")
+            }, 
+            TextAlignment { 
+                vertical: VerticalAlign::Center, 
+                horizontal: HorizontalAlign::Center 
+            }
+        ),
+        transform: Transform {
+            translation: Vec3::new(0.0, 0.0, 11.0),
+            ..default()
+        },
+        ..default()
+    })
+    .insert(SceneText);
+
+    let window_width = window.width;
+    let window_height = window.height;
     commands.spawn_bundle(
         SpriteBundle {
             sprite: Sprite {
@@ -48,7 +108,7 @@ fn setup(mut commands: Commands, windows: Res<Windows>) {
             },
             transform: Transform {
                 scale: Vec3::new(96.0, 96.0, 10.0),
-                translation: Vec3::new(-700.0, 100.0 - window.width()/4.0, 10.0),
+                translation: Vec3::new(-window_width / 2.0, 100.0 - window_height / 4.0, 10.0),
                 ..default()
             },
             ..default()
@@ -69,15 +129,10 @@ fn transforming(mut query: Query<&mut Transform, With<Protagonist>>) {
     }
 }
 
-// fn resize_notificator(resize_event: Res<Events<WindowResized>>) {
-//     let mut reader = resize_event.get_reader();
-//     for e in reader.iter(&resize_event) {
-//         println!("width = {} height = {}", e.width, e.height);
-//     }
-// }
-fn add_road(windows: Res<Windows>, mut commands: Commands) {
-    let window = windows.get_primary().unwrap();
-    println!("Window size was: {},{}", window.width(), window.height());
+fn add_road(mut commands: Commands, window: Res<Window>) {
+    let window_width = window.width;
+    let window_height = window.height;
+    println!("Window size was: {},{}", window_width, window_height);
     commands.spawn_bundle(
         SpriteBundle {
             sprite: Sprite {
@@ -85,8 +140,8 @@ fn add_road(windows: Res<Windows>, mut commands: Commands) {
                 ..default()
             },
             transform: Transform { 
-                scale: Vec3::new(window.width() * 2.0, 100.0, 10.0),
-                translation: Vec3::new(0.0, -window.height()/4.0, 0.0),
+                scale: Vec3::new(window_width, 100.0, 10.0),
+                translation: Vec3::new(0.0, -window_height/4.0, 0.0),
                 ..default()
             },
             ..default()
@@ -153,4 +208,33 @@ fn add_beggar(mut commands: Commands) {
         }
     )
     .insert(Beggar);
+}
+
+fn beggar_movements(
+    prota_posi: Query<(&Transform, &Protagonist), Without<Beggar>>, 
+    mut beggar_posi: Query<&mut Transform, With<Beggar>>,
+    despawner: Query<Entity, With<Beggar>>,
+    mut commands: Commands,
+    mut text: Query<&mut Text, With<SceneText>>
+) {
+    for (ppos, _) in prota_posi.iter() {
+        if ppos.translation.x > -300.0 {
+            for mut bpos in beggar_posi.iter_mut() {
+                if bpos.translation.x > -300.0 {
+                    text.iter_mut().next().unwrap().sections[0].value = "I better run".to_string();
+                    bpos.translation.x -= 2.0;
+                    return;
+                } 
+                if bpos.translation.x <= -300.0 && bpos.translation.y < 100.0 {
+                    text.iter_mut().next().unwrap().sections[0].value = "".to_string();
+                    bpos.translation.y += 1.0;
+                    return;
+                }
+                
+                for entity in despawner.iter() {
+                    commands.entity(entity).despawn();
+                }
+            }
+        }
+    }
 }
